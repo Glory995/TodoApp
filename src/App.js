6b1632a04eac7aa843eff5086e6d9f52f82ec736
Glory <glorysimon995@gmail.com>
2023-06-15 16:05:55 +0100

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-
-import './App.css';
-import 'remixicon/fonts/remixicon.css'
-
-import List from './Component/List';
-import Alert from './Component/Alert';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
-    const {name, setname}= useState("");
-    const {list, setlist}= useState({});
-    const {isEditing, setisEditing}= useState("false");
-    const {editId, seteditId}= useState("null");
-    const {alert , setalert}= useState({ show:false , msg:"", type:""});
+  const [inputValue, setInputValue] = useState('');
+  const [listItems, setListItems] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleInputChange= (e) =>{
+          setInputValue(e.target.value);
+
+  }
+
+const handleAddItem = ()=>{
+
+  
+  if ( inputValue.trim() !=''){
+    if(editIndex !==-1){
+
+      const updatedItems = [...listItems];
+      updatedItems[editIndex] =inputValue;
+      setListItems(updatedItems);
+      setEditIndex(-1);
+
+
+    }
+
+    else{
+      setListItems([inputValue, ...listItems ]);
+    }
+
+    setInputValue('')
+}
+}
+
+
+  const handleEditItem =(index) =>{
+
+    setInputValue(listItems[index])
+    setEditIndex(index)
+
+
+  }
+
+
+  const HandleDeleteItem =(index)=>{
+
+    const updatedItems = [...listItems];
+    updatedItems.splice(index, 1);
+    setListItems(updatedItems);
+
+
+  }
+
+
+
+
 
   return (
-      <section className='section-center'>
+    <div  className='wrapper'>
 
-       
+        <div className="header">Todo List</div>
 
-      </section>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button className='addbtn' onClick={handleAddItem}> {editIndex === -1 ? 'Add' : 'Save'}</button>
 
-   );
+      <div className='holdinglist'>
+      <ul>
+        {listItems.map((item, index) => (
+          <li   className='todolis' key={index}>
+            <div className='item'>
+                {item}
+            </div>
+            
+            <div  >
+
+              
+            <button className='editbtn' onClick={() => handleEditItem(index)}>Edit</button>
+            
+
+            <button  className='editbtn' onClick={() => HandleDeleteItem(index)}> delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      </div>
+    </div>
+  );
 }
 
 export default App;
